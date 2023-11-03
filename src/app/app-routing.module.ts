b5cc59e3267773a4dc256194ebe407db4519ee12
Salome Brack <salome.brack@gmx.de>
2023-11-03 20:47:15 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { ErrorComponent } from './error/error.component';
+
+const defaultImage: string = 'Candy Land';
+const name: string = 'Salome Brack';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, title: name, data: { animation: 'home' } },
+  { path: 'portfolio', redirectTo: `/portfolio/${defaultImage}`, pathMatch: 'full' },
+  { path: 'portfolio/:title', component: PortfolioComponent, data: { animation: 'portfolio' } },
+  { path: 'about', component: AboutComponent, title: `About Me | ${name}`, data: { animation: 'about' } },
+  { path: 'contact', component: ContactComponent, title: `Contact | ${name}`, data: { animation: 'contact' } },
+  { path: 'imprint', component: ImprintComponent, title: `Imprint | ${name}`, data: { animation: 'imprint' } },
+  { path: '404', component: ErrorComponent, title: 'Page not found' },
+  { path: '**', redirectTo: '404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ListComponent } from './portfolio/list/list.component';
+import { ViewComponent } from './portfolio/view/view.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { ErrorComponent } from './error/error.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PortfolioComponent,
+    ListComponent,
+    ViewComponent,
+    AboutComponent,
+    ContactComponent,
+    ImprintComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSnackBarModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
