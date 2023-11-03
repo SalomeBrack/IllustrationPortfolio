bcc65800c8394274fdadd0c4de3356df1ab9fe7d
Salome Brack <salome.brack@gmx.de>
2023-11-03 23:40:57 +0100

    about and contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  value = 'salomebrack';
+  domain = '@gmail.com';
 
+  constructor(private snackBar: MatSnackBar, private clipboard: Clipboard) { }
+
+  redirect() {
+    window.location.href = 'mailto:' + this.getValue(this.value) + this.domain;
+  }
+
+  copy() {
+    const successful = this.clipboard.copy(this.getValue(this.value) + this.domain);
+
+    if (successful) {
+      this.snackBar.open('"' + this.getValue(this.value) + this.domain + '" copied', '', {
+        duration: 2000
+      });
+    }
+  }
+
+  getValue(v: string) {
+    return v + '.illustration';
+  }
 }
